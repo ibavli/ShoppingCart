6ff32a33c0f801ecf4e312eb97cc8ba0c5d79b00
ibavli <ibrahim.bavli35@gmail.com>
2019-03-27 14:44:55 +0300

    Product ve Category entitileri oluşturuldu.

---
+++ ShoppingCart.Entities/CategoryEntities/Category.cs
@@ -0,0 +1,30 @@
+﻿using ShoppingCart.Entities.ProductEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Entities.CategoryEntities
+{
+    public class Category : BaseEntity
+    {
+        #region Constructor
+
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+
+        #endregion
+
+        public string Title { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public virtual Category Parent { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
+
